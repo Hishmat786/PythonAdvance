0c2d0c0c5a6ccdfc95988db09d513aea79a751e8
Hishmat786 <hishmatdrawarh786@gmail.com>
2024-08-14 23:25:01 +0500

    added logical operator

---
+++ LogicalOper.py
@@ -0,0 +1,17 @@
+a = 10
+b = 20
+
+# Check if both conditions are true
+if a > 5 and b < 30:
+    print("Both conditions are true.")
+else:
+    print("At least one condition is false.")
+
+
+temperature = 25
+weather = "sunny"
+
+if (temperature > 20 and temperature < 30) or weather == "sunny":
+    print("It's a good day for outdoor activities.")
+else:
+    print("Maybe stay indoors.")
