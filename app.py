5198b65a58a570e092550d25d2f54d12248bef9a
Hishmat786 <hishmatdrawarh786@gmail.com>
2024-08-14 17:45:43 +0500

    string practice

---
+++ app.py
@@ -0,0 +1,9 @@
+age = int(input('Enter your age: '))
+print(type(age))
+if age > 18:
+    print('you are edult')
+else:
+    print('you are younger')
+
+course= "Python's course for biginners"
+print(course)
