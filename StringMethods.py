53630cf66c3a95ed08d835e8cf9e7349dc9b1f2e
Hishmat786 <hishmatdrawarh786@gmail.com>
2024-08-14 18:10:02 +0500

    added string methods

---
+++ StringMethods.py
@@ -0,0 +1,12 @@
+msg= "Hello there is Programmer"
+print(len(msg))
+print(msg.upper())
+print(msg.capitalize())
+print(msg.lower())
+print(msg.find('H'))
+print(msg.find('h'))
+
+print(msg.replace('is', 'are'))
+
+print('Hello' in msg)
+print('hello' in msg)
