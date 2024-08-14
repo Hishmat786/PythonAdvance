e8bca881eda486590ea83b958519d780e78b2cf7
Hishmat786 <hishmatdrawarh786@gmail.com>
2024-08-14 23:05:32 +0500

    math function module added

---
+++ MathFunction.py
@@ -0,0 +1,13 @@
+import math
+
+x= 2.8
+y= 2.5
+z= 2.4
+print (round(x) ,round(y) ,round(z)) # round function
+
+print(abs(-2.5))    # absolute value
+
+print(math.ceil(2.9))       # math ceil and floor function  
+print(math.floor(2.9))
+# https://www.w3schools.com/python/module_math.asp
+# https://docs.python.org/3/library/math.html
